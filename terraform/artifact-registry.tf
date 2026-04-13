a8d81ce8648f6e37791cefe6d00a9aeb4a0990a8
saleel <saleel@saleel.xyz>
2026-04-13 18:47:55 +0400

    chore: make docker public

--- terraform/artifact-registry.tf
+++ terraform/artifact-registry.tf
@@ -6,3 +6,10 @@ resource "google_artifact_registry_repository" "proof_verifier" {
 
   depends_on = [google_project_service.artifactregistry]
 }
+
+resource "google_artifact_registry_repository_iam_member" "public_reader" {
+  repository = google_artifact_registry_repository.proof_verifier.name
+  location   = var.region
+  role       = "roles/artifactregistry.reader"
+  member     = "allUsers"
+}
